19b6984f32d3f4fa6605a82fb42b30ade9326c8d
noname <noname@android.1.4>
2020-04-19 06:45:23 +0900

    push

---
+++ notes.py
@@ -0,0 +1,87 @@
+# !/usr/bin/python3
+# coding=utf-8
+from tkinter import *
+import pickle
+from ewmh import EWMH
+import tkinter as tk
+
+#название файла, куда буду сохранять текст заметок и туда же буду его сохранять
+notesdata = 'notes.data'
+placedata = 'place.data'
+
+#загружаю текст из файла notesdata
+f=open(notesdata, 'rb')
+storedtext = pickle.load(f)
+
+f=open(placedata, 'rb')
+coordinates = pickle.load(f)
+
+#создаю функцию, которая захватывает текст и сохраняет его в notes.data
+def SaveText():
+    s = text.get(1.0, END)
+    label['text'] = s
+    notestext = s
+    f=open(notesdata, 'wb')
+    pickle.dump(notestext, f)
+    coordinates = root.geometry()
+    f=open(placedata, 'wb')
+    pickle.dump(coordinates, f)
+    f.close()
+
+#создаю событие, которое будет выполнять функцию SaveText, при нажатии CTRL+S
+def CtrlS(event):
+    root.after(10, ctrl_s, event.widget)
+
+def ctrl_s(widget):
+    SaveText()
+
+#создаю событие, которое будет выполнять SaveText, при закрытии программы
+def on_closing():
+    SaveText()
+    root.destroy()
+
+#!!
+ewmh = EWMH()
+
+#создаю окно приложения
+root = tk.Tk()
+root.title("")
+root.geometry(coordinates)
+
+root.resizable(False, False)
+root.iconphoto(False, tk.PhotoImage(file='favicon.png'))
+root.lift()
+#root.wm_attributes("-alpha", 0.8)
+
+
+
+#один из способов установить свою иконку, не работает бля!
+#root.iconbitmap('favicon/favicon.gif')
+
+#!!
+root.update_idletasks()     # to make sure the window is displayed
+w = ewmh.getActiveWindow()  # get the window
+ewmh.setWmState(w, 1, '_NET_WM_STATE_SKIP_TASKBAR')  # remove window from taskbar
+ewmh.display.flush()
+
+#временное решение против сворачивания крови и окна
+#root.bind('<Unmap>', lambda event: event.widget.deiconify())
+
+text = Text(width=40, height=40, font='Arial 9')
+text.pack()
+
+frame = Frame()
+frame.pack()
+
+
+area=storedtext
+
+label = Label()
+label.pack()
+
+
+text.insert(1.0, area)
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+root.bind('<Control-s>',ctrl_s)
+root.mainloop()
