a8e08acc9d6ce33a863f0002da90f1da863aa44f
microdot178 <microdot178@tuta.io>
2020-05-26 00:27:11 +0000

    data in json

--- notes.py
+++ notes.py
@@ -1,32 +1,43 @@
 # !/usr/bin/python3
 # coding=utf-8
 from tkinter import *
-import pickle
+#import pickle
 from ewmh import EWMH
 import tkinter as tk
+import json
 
 #название файла, откуда буду брать текст заметок и туда же буду его сохранять
-notesdata = 'notes.data'
-placedata = 'place.data'
+#notesdata = 'notes.data'
+#placedata = 'place.data'
 
 #загружаю текст из файла notesdata
-f=open(notesdata, 'rb')
-storedtext = pickle.load(f)
+#f=open(notesdata, 'rb')
+#storedtext = pickle.load(f)
 
-f=open(placedata, 'rb')
-coordinates = pickle.load(f)
+#f=open(placedata, 'rb')
+#coordinates = pickle.load(f)
+
+with open('sw_templates.json') as f: # теперь сохраняться и загружаться будет в json
+    data = json.load(f)
+
+notestext = data['notestext']
+coordinates = data['coordinates']
 
 #создаю функцию, которая захватывает текст и сохраняет его в notes.data
 def SaveText():
     s = text.get(1.0, END)
     label['text'] = s
     notestext = s
-    f=open(notesdata, 'wb')
-    pickle.dump(notestext, f)
+    #f=open(notesdata, 'wb')
+    #pickle.dump(notestext, f)
     coordinates = root.geometry()
-    f=open(placedata, 'wb')
-    pickle.dump(coordinates, f)
-    f.close()
+    #f=open(placedata, 'wb')
+    #pickle.dump(coordinates, f)
+    #f.close()
+    data = {'coordinates': coordinates, 'notestext': notestext} # json 
+    with open('sw_templates.json', 'w') as f:                   # json
+        f.write(json.dumps(data))                               # json
+
 
 #создаю событие, которое будет выполнять функцию SaveText, при нажатии CTRL+S
 def CtrlS(event):
@@ -67,14 +78,14 @@ ewmh.display.flush()
 #временное решение против сворачивания крови и окна
 #root.bind('<Unmap>', lambda event: event.widget.deiconify())
 
-text = Text(width=40, height=40, font='Arial 9')
+text = Text(width=40, height=40, font='Arial 11')
 text.pack()
 
 frame = Frame()
 frame.pack()
 
 
-area=storedtext
+area=notestext
 
 label = Label()
 label.pack()
